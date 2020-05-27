b03c25eb2128eb1091d9cac8b365994b5fdc32b2
mpaolodr <mpaolodr_1504@yahoo.com>
2020-05-27 17:12:15 -0400

    Updates

---
+++ precache-manifest.16f6d00a1f07657b8eeaa4d513cace5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b817f8521a4d08408e743d479425a1e1",
+    "url": "/%3Cgithub-repo-name%3E/index.html"
+  },
+  {
+    "revision": "a1951a3a26d81b12bad6",
+    "url": "/%3Cgithub-repo-name%3E/static/css/main.f5b33359.chunk.css"
+  },
+  {
+    "revision": "2073a9d42a8ccd9c280b",
+    "url": "/%3Cgithub-repo-name%3E/static/js/2.81526a1c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/%3Cgithub-repo-name%3E/static/js/2.81526a1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1951a3a26d81b12bad6",
+    "url": "/%3Cgithub-repo-name%3E/static/js/main.947eb991.chunk.js"
+  },
+  {
+    "revision": "269473f8b3c672224a16",
+    "url": "/%3Cgithub-repo-name%3E/static/js/runtime-main.80232dee.js"
+  }
+]);
