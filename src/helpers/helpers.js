c7f973c5704a4dfe82996d55f24c61fd1583f417
mpaolodr <mpaolodr_1504@yahoo.com>
2020-05-26 21:12:45 -0400

    created helper function to generate grid with random values

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -11,6 +11,16 @@ export function generateGrid(r, c) {
   return rows;
 }
 
+export function generateRandom(r, c) {
+  const rows = [];
+
+  for (let i = 0; i < r; i++) {
+    rows.push(Array.from(Array(c), () => (Math.random() > 0.8 ? 1 : 0)));
+  }
+
+  return rows;
+}
+
 export function stateChange(rowIndex, colIndex, cb, grid) {
   const newGrid = produce(grid, (gridCopy) => {
     gridCopy[rowIndex][colIndex] = grid[rowIndex][colIndex] ? 0 : 1;
